65cad497c00af79ccfbe583bdc1f217df8247574
Venu Virparia <129399359+VenuVirparia@users.noreply.github.com>
2025-05-01 23:16:47 +0530

    Create parent->child.c

---
+++ parent->child.c
@@ -0,0 +1,30 @@
+#include<unistd.h> //fork
+#include<sys/types.h> //pid_t
+#include<stdio.h> //printf
+#include<stdlib.h> //exit
+#include<sys/wait.h> //wait
+
+
+int main()
+{
+  pid_t id;
+  id = fork();
+  if(id == 0)
+  {	
+    sleep(2);
+    printf("Child executed: ");
+    printf("%d \n",id);
+    printf("Process id child %d,%d\n\n\n",getpid(),getppid());
+  }
+  else if(id > 0)
+  {
+    printf("Parent executed: ");
+    printf("%d \n",id);
+    printf("Process id parent %d,%d\n\n\n",getpid(),getppid());
+  }
+  else if(id == -1)
+  {
+    printf("error\n");
+  }
+  exit(0);
+}
